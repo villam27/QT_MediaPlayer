829aaadc7d867af96843b7468905ce67ce79f59c
villam27 <villam2703@protonmail.com>
2023-10-29 01:37:17 +0200

    [Add] Resize

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setFixedSize(WIN_WIDTH, WIN_HEIGHT);
+    //w.setFixedSize(WIN_WIDTH, WIN_HEIGHT);
     w.show();
     return a.exec();
 }
