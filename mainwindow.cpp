d977c4958c1239f4bb45b1d9bbca53e43f8460e2
villam27 <villam2703@protonmail.com>
2023-10-29 01:22:54 +0200

    [Add] Working slider

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,8 +18,10 @@ MainWindow::MainWindow(QWidget *parent)
     setCentralWidget(centralWidget);
     _video->setGeometry(20, 30, WIN_WIDTH - 40, WIN_HEIGHT - 100);
     _player->setVideoOutput(_video);
-    //_videoSlider->setRange(0, _player->duration() / 1000);
     _videoSlider->setFixedWidth(WIN_WIDTH - 40);
+    _videoSlider->setTracking(true);
+    _videoSlider->setTickInterval(1);
+    _videoSlider->setTickPosition(QSlider::TicksBelow);
     _menuBar->setMediaPlayer(_player);
     _mediaPlayerButton->setMediaPlayer(_player);
 
@@ -27,6 +29,10 @@ MainWindow::MainWindow(QWidget *parent)
                    , this, &MainWindow::onMediaStatusChanged);
     connect(_player, &QMediaPlayer::playbackStateChanged
                    , this, &MainWindow::onPlaybackStateChanged);
+    connect(_player, &QMediaPlayer::positionChanged
+                   , this, &MainWindow::onPositionChanged);
+    connect(_videoSlider, &QSlider::valueChanged
+                        , this, &MainWindow::onValueChanged);
 
     _layout->setMenuBar(_menuBar);
     _layout->addWidget(_video);
@@ -50,7 +56,11 @@ MainWindow::~MainWindow()
 void MainWindow::onMediaStatusChanged(QMediaPlayer::MediaStatus status)
 {
     if (status == QMediaPlayer::LoadedMedia)
+    {
+        _player->setPosition(0);
         _player->play();
+        _videoSlider->setRange(0, _player->duration() / 1000);
+    }
 }
 
 void MainWindow::onPlaybackStateChanged(QMediaPlayer::PlaybackState state)
@@ -58,4 +68,14 @@ void MainWindow::onPlaybackStateChanged(QMediaPlayer::PlaybackState state)
     std::cout << "Changed playback state "
               << QString::number(state).toStdString()
               << std::endl;
+}
+
+void MainWindow::onPositionChanged(qint64 position)
+{
+    _videoSlider->setValue(position / 1000);
+}
+
+void MainWindow::onValueChanged(int value)
+{
+    _player->setPosition(value * 1000);
 }
