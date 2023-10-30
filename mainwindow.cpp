0718dcf089fde71ac588600f19dec3eaad677300
villam27 <villam2703@protonmail.com>
2023-10-30 20:15:26 +0100

    [Add] Audio Output

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <iostream>
+#include <QAudioOutput>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,14 +11,16 @@ MainWindow::MainWindow(QWidget *parent)
     QWidget *centralWidget = new QWidget(this);
     _layout = new QVBoxLayout(centralWidget);
     _video = new QVideoWidget(this);
+    _audio = new QAudioOutput;
     _menuBar = new CustomMenuBar(this);
     _videoSlider = new QSlider(Qt::Horizontal, this);
     _mediaPlayerButton = new MediaPlayerButton(this);
-    _player = new QMediaPlayer(this);
+    _player = new QMediaPlayer;
 
     setCentralWidget(centralWidget);
     _video->setGeometry(20, 30, WIN_WIDTH - 40, WIN_HEIGHT - 100);
     _player->setVideoOutput(_video);
+    _player->setAudioOutput(_audio);
     _videoSlider->setFixedWidth(WIN_WIDTH - 40);
     _videoSlider->setTracking(true);
     _videoSlider->setTickInterval(1);
@@ -44,6 +47,7 @@ MainWindow::~MainWindow()
 {
     delete ui;
     delete _video;
+    delete _audio;
     delete _menuBar;
     delete _mediaPlayerButton;
     delete _layout;
