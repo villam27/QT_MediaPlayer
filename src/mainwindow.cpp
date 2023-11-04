991b4d52a343c133121e62967d49f0e03bd16dcd
villam27 <villam2703@protonmail.com>
2023-11-04 20:23:27 +0100

    [Add] Style and res

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -25,6 +25,7 @@ MainWindow::MainWindow(QWidget *parent)
     _videoSlider->setTracking(true);
     _videoSlider->setTickInterval(1);
     _videoSlider->setTickPosition(QSlider::TicksBelow);
+    _videoSlider->setDisabled(true);
     _menuBar->setMediaPlayer(_player);
     _mediaPlayerButton->setMediaPlayer(_player);
     _mediaPlayerButton->setAudioOutput(_audio);
@@ -63,6 +64,7 @@ void MainWindow::onMediaStatusChanged(QMediaPlayer::MediaStatus status)
 {
     if (status == QMediaPlayer::LoadedMedia)
     {
+        _videoSlider->setDisabled(false);
         _player->setPosition(0);
         _player->play();
         _videoSlider->setRange(0, _player->duration() / 1000);
